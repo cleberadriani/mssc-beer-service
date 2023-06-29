19a3986defc813335ad445668f5ac5439eb9c6a0
Cleber Adriani <cleberadriani@gmail.com>
2023-06-29 17:48:19 -0300

    testes unitários

---
+++ src/test/java/guru/springframework/msscbeerservice/web/controller/BeerControllerTest.java
@@ -0,0 +1,34 @@
+package guru.springframework.msscbeerservice.web.controller;
+
+import java.util.UUID;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(BeerController.class)
+public class BeerControllerTest {
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    void testGetBeerById() throws Exception {
+        mockMvc.perform(get("/api/v1/beer/" + UUID.randomUUID().toString()).accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+        
+    }
+
+    @Test
+    void testSaveNewBeer() {
+
+    }
+
+    @Test
+    void testUpdateBeerById() {
+
+    }
+}
